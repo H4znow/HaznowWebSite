040284ce860853d0895de13a8bf8efc2dbe6a0b6
Amine Haddou <haddouamine2002@gmail.com>
2021-09-29 19:23:03 +0300

    Mise a jour page Creations de : Animations bloc et boutons envoyer

--- js/creations.js
+++ js/creations.js
@@ -5,59 +5,88 @@ const creation4 = document.getElementById("creation4");
 const creation5 = document.getElementById("creation5");
 const creation6 = document.getElementById("creation6");
 const creation7 = document.getElementById("creation7");
+const creations = [creation1, creation2, creation3, creation4, creation5, creation6, creation7];
 creation1.addEventListener('mouseenter', e => {
     creation1.style.boxShadow = "6px 6px 10px #3e00ba";
-    creation1.style.border = "3px solid #20F7E2";
+    creation1.style.border = "3px solid #474747";
+    creation1.style.width = "200px";
+    creation1.style.height = "300px";
 })
 creation1.addEventListener('mouseleave', e =>{
     creation1.style.boxShadow = "2px 5px 10px black";
     creation1.style.border = "none";
+    creation1.style.width = "150px";
+    creation1.style.height = "250px";
 })
 creation2.addEventListener('mouseenter', e => {
     creation2.style.boxShadow = "6px 6px 10px #3e00ba";
-    creation2.style.border = "3px solid #20F7E2"
+    creation2.style.border = "3px solid #474747";
+    creation2.style.width = "200px";
+    creation2.style.height = "300px";
 })
 creation2.addEventListener('mouseleave', e =>{
     creation2.style.boxShadow = "2px 5px 10px black";
     creation2.style.border = "none";
+    creation2.style.width = "150px";
+    creation2.style.height = "250px";
 })
 creation3.addEventListener('mouseenter', e => {
     creation3.style.boxShadow = "6px 6px 10px #3e00ba";
-    creation3.style.border = "3px solid #20F7E2";
+    creation3.style.border = "3px solid #474747";
+    creation3.style.width = "200px";
+    creation3.style.height = "300px";
 })
 creation3.addEventListener('mouseleave', e =>{
     creation3.style.boxShadow = "2px 5px 10px black";
     creation3.style.border = "none";
+    creation3.style.width = "150px";
+    creation3.style.height = "250px";
 })
 creation4.addEventListener('mouseenter', e => {
     creation4.style.boxShadow = "6px 6px 10px #3e00ba";
-    creation4.style.border = "3px solid #20F7E2";
+    creation4.style.border = "3px solid #474747";
+    creation4.style.width = "200px";
+    creation4.style.height = "300px";
 })
 creation4.addEventListener('mouseleave', e =>{
     creation4.style.boxShadow = "2px 5px 10px black";
     creation4.style.border = "none";
+    creation4.style.width = "150px";
+    creation4.style.height = "250px";
 })
 creation5.addEventListener('mouseenter', e => {
     creation5.style.boxShadow = "6px 6px 10px #3e00ba";
-    creation5.style.border = "3px solid #20F7E2"
+    creation5.style.border = "3px solid #474747";
+    creation5.style.width = "200px";
+    creation5.style.height = "300px";
 })
 creation5.addEventListener('mouseleave', e =>{
     creation5.style.boxShadow = "2px 5px 10px black";
     creation5.style.border = "none";
+    creation5.style.width = "150px";
+    creation5.style.height = "250px";
 })
 creation6.addEventListener('mouseenter', e => {
     creation6.style.boxShadow = "6px 6px 10px #3e00ba";
-    creation6.style.border = "3px solid #20F7E2"
+    creation6.style.border = "3px solid #474747";
+    creation6.style.width = "200px";
+    creation6.style.height = "300px";
 })
 creation6.addEventListener('mouseleave', e =>{
     creation6.style.boxShadow = "2px 5px 10px black";
     creation6.style.border = "none";
+    creation6.style.width = "150px";
+    creation6.style.height = "250px";
 })
 creation7.addEventListener('mouseenter', e => {
     creation7.style.boxShadow = "6px 6px 10px #3e00ba";
-    creation7.style.border = "3px solid #20F7E2"
+    creation7.style.border = "3px solid #474747";
+    creation7.style.width = "200px";
+    creation7.style.height = "300px";
 })
-creation6.addEventListener('mouseleave', e =>{
+creation7.addEventListener('mouseleave', e =>{
     creation7.style.boxShadow = "2px 5px 10px black";
     creation7.style.border = "none";
+    creation7.style.width = "150px";
+    creation7.style.height = "250px";
 })
