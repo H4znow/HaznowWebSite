11114223ab4a25e1235efba602322e7b91233592
Amine Haddou <haddouamine2002@gmail.com>
2021-09-30 18:50:35 +0300

    Mise a jour page acceuil et page 1 mise en page

--- js/scriptP1.js
+++ js/scriptP1.js
@@ -4,7 +4,11 @@ const text1 = document.getElementById("text1");
 const bloc2 = document.getElementById("bloc2");
 const text2 = document.getElementById("text2");
 const img2 = document.getElementById("img2");
+const mediaQuery = window.matchMedia('all and (max-device-width: 480px)')
 img1.addEventListener('mouseenter', e => {
+    if (mediaQuery.matches) {
+        return;
+    }
     text1.style.display= "block";
     img1.style.display= "block";
     img1.style.marginRight = "60px";
@@ -16,6 +20,9 @@ img1.addEventListener('mouseenter', e => {
     bloc1.style.paddingTop= "20px";
 })
 bloc1.addEventListener("mouseleave", e =>{
+    if (mediaQuery.matches) {
+        return;
+    }
     setTimeout(() => {
         text1.style.display= "none";
         img1.style.boxShadow = "6px 6px 40px black";
@@ -28,6 +35,9 @@ bloc1.addEventListener("mouseleave", e =>{
 //Bloc2 animation
 
 img2.addEventListener('mouseenter', e => {
+    if (mediaQuery.matches) {
+        return;
+    }
     text2.style.display= "block";
     img2.style.display= "block";
     img2.style.marginRight = "60px";
@@ -40,6 +50,9 @@ img2.addEventListener('mouseenter', e => {
     
 })
 bloc2.addEventListener("mouseleave", e =>{
+    if (mediaQuery.matches) {
+        return;
+    }
     setTimeout(() => {
         text2.style.display= "none";
         img2.style.boxShadow = "6px 6px 40px black";
@@ -47,3 +60,24 @@ bloc2.addEventListener("mouseleave", e =>{
         img2.style.marginRight = "0px";    
     }, 200)
 })
+if (mediaQuery.matches) {
+    text1.style.display= "block";
+    text2.style.display= "block";
+    text2.style.marginBottom="10px";
+
+    img1.style.display= "block";
+    img2.style.display= "block";
+
+    img1.style.width = "500px";
+    img2.style.width = "500px";
+    
+    bloc1.style.display="flex";
+    bloc1.style.flexDirection= "column";
+    bloc1.style.justifyContent= "center";
+    bloc1.style.alignItems= "center";
+    bloc2.style.display="flex";
+    bloc2.style.flexDirection= "column-reverse";
+    bloc2.style.justifyContent= "center";
+    bloc2.style.alignItems= "center";
+
+}
